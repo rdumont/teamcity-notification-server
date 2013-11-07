8075a384148dfc3dd3e31c42b0e97b1c5c315acb
Rodrigo Dumont <rlsdumont@gmail.com>
2013-11-07 21:42:59 -0200

    Read settings from file

--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace TeamCityNotifier.NotificationServer
+﻿using System;
+
+namespace TeamCityNotifier.NotificationServer
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var settings = ServerSettings.ReadFrom("config.yaml");
+
+            Console.WriteLine("Port: {0}", settings.Port);
         }
     }
 }
---
+++ src/Server/ServerSettings.cs
@@ -0,0 +1,20 @@
+﻿using System.IO;
+using System.Text;
+using YamlDotNet.RepresentationModel.Serialization;
+using YamlDotNet.RepresentationModel.Serialization.NamingConventions;
+
+namespace TeamCityNotifier.NotificationServer
+{
+    class ServerSettings
+    {
+        [YamlAlias("port")]
+        public int Port { get; set; }
+
+        public static ServerSettings ReadFrom(string filePath)
+        {
+            var deserializer = new Deserializer(namingConvention: new CamelCaseNamingConvention());
+            var reader = new StreamReader(filePath, Encoding.UTF8);
+            return deserializer.Deserialize<ServerSettings>(reader);
+        }
+    }
+}
