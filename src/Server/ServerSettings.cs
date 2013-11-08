e8198e18ed66cb001edc460ce9c71fd454334b2f
Rodrigo Dumont <rlsdumont@gmail.com>
2013-11-08 16:44:39 -0200

    Update settings to include TeamCity server info

--- src/Server/ServerSettings.cs
+++ src/Server/ServerSettings.cs
@@ -10,11 +10,26 @@ namespace TeamCityNotifier.NotificationServer
         [YamlAlias("port")]
         public int Port { get; set; }
 
+        [YamlAlias("teamCity")]
+        public TeamCitySettings TeamCity { get; set; }
+
         public static ServerSettings ReadFrom(string filePath)
         {
             var deserializer = new Deserializer(namingConvention: new CamelCaseNamingConvention());
             var reader = new StreamReader(filePath, Encoding.UTF8);
             return deserializer.Deserialize<ServerSettings>(reader);
         }
+    }
+
+    class TeamCitySettings
+    {
+        [YamlAlias("url")]
+        public string Url { get; set; }
+
+        [YamlAlias("username")]
+        public string Username { get; set; }
+
+        [YamlAlias("password")]
+        public string Password { get; set; }
     }
 }
