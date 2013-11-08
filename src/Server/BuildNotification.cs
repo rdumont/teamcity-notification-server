865a285cd39932ff0bf1ebfc43d5198fc63720cc
Rodrigo Dumont <rlsdumont@gmail.com>
2013-11-08 18:23:07 -0200

    Notify connections on build change

---
+++ src/Server/BuildNotification.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using TeamCityNotifier.NotificationServer.TeamCity;
+
+namespace TeamCityNotifier.NotificationServer
+{
+    public class BuildNotification
+    {
+        [JsonProperty(PropertyName = "eventType")]
+        public string EventType { get; set; }
+
+        [JsonProperty(PropertyName = "build")]
+        public Build Build { get; set; }
+
+        public BuildNotification(string eventType, Build build)
+        {
+            EventType = eventType;
+            Build = build;
+        }
+    }
+}
--- src/Server/Program.cs
+++ src/Server/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR;
 using Microsoft.Owin.Hosting;
 using TeamCityNotifier.NotificationServer.TeamCity;
 
@@ -24,14 +25,25 @@ namespace TeamCityNotifier.NotificationServer
                 var poller = new BuildsPoller(settings.TeamCity.Url, settings.TeamCity.Username,
                     settings.TeamCity.Password, TimeSpan.FromSeconds(1));
 
+                var connectionGroups = GlobalHost.ConnectionManager.GetConnectionContext<BuildsConnection>().Groups;
+
                 poller.BuildStarted += build =>
+                {
+                    connectionGroups.Send(build.BuildTypeId, new BuildNotification("start", build));
                     Console.WriteLine("Build started: {0}, {1}%", build.BuildTypeId, build.PercentageComplete);
+                };
 
                 poller.BuildUpdated += build =>
+                {
+                    connectionGroups.Send(build.BuildTypeId, new BuildNotification("update", build));
                     Console.WriteLine("Build updated: {0}, {1}%", build.BuildTypeId, build.PercentageComplete);
+                };
 
                 poller.BuildStopped += build =>
+                {
+                    connectionGroups.Send(build.BuildTypeId, new BuildNotification("stop", build));
                     Console.WriteLine("Build stopped: {0}", build.BuildTypeId);
+                };
 
                 await poller.StartAsync();
             }
